9d46a421e7dcb42180280b4c5c5a27ed6379db59
Jeremy Linux Workstation <jeremyzelaya@hotmail.es>
2020-01-02 22:39:21 -0600

    Finished class on integrating Async/Await into Task-Manager playgound

--- Courses/Nodejs Bootcamp/Task-Manager/Playground/promise-chaining.js
+++ Courses/Nodejs Bootcamp/Task-Manager/Playground/promise-chaining.js
@@ -2,16 +2,33 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5e0d463a985e9d555243e103', {
-    age: 1
-}).then(user => {
-    console.log(user);
-    return User.countDocuments({age:1})
-}).then(result => {
-    console.log(result);
+// User.findByIdAndUpdate('5e0d463a985e9d555243e103', {
+//     age: 1
+// }).then(user => {
+//     console.log(user);
+//     return User.countDocuments({age:1})
+// }).then(result => {
+//     console.log(result);
     
-}).catch(e => {
-    console.log(e);
+// }).catch(e => {
+//     console.log(e);
+    
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments({age})
+    return {
+        user,
+        count
+    }
+}
+updateAgeAndCount('5e0d463a985e9d555243e103',2).then(result => {
+    console.log(result);
+}).catch(error => {
+    console.log(error);
     
 })
+
+
 
--- Courses/Nodejs Bootcamp/Task-Manager/Playground/promise-chaining2.js
+++ Courses/Nodejs Bootcamp/Task-Manager/Playground/promise-chaining2.js
@@ -1,17 +1,30 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e0affd2eca8bc26a105c54d').then(task => {
-    if (!task) {
-        console.log('No task was found');
-    }
-    console.log(task);
-    return Task.countDocuments({
-        completed: false
-    })
-}).then(result => {
-    console.log(result, ' tasks are uncompleted');
-}).catch(e => {
-    console.log(e);
+// Task.findByIdAndDelete('5e0affd2eca8bc26a105c54d').then(task => {
+//     if (!task) {
+//         console.log('No task was found');
+//     }
+//     console.log(task);
+//     return Task.countDocuments({
+//         completed: false
+//     })
+// }).then(result => {
+//     console.log(result, ' tasks are uncompleted');
+// }).catch(e => {
+//     console.log(e);
+// })
+
+const findAndDeleteById = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed: false})
+    return count
+}
+
+findAndDeleteById('5e0ec3129e6602a5e763ac85').then(count => {
+    console.log(count);
+    
+}).catch(error => {
+    console.log(error);
     
 })
