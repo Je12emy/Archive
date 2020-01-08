078b867ac27cddf57c1f0584f3c3831b606826e1
Jeremy Linux Workstation <jeremyzelaya@hotmail.es>
2020-01-08 01:38:48 -0600

    Finished auth integration in Task router

--- Courses/Nodejs Bootcamp/Task-Manager/src/Routers/task.js
+++ Courses/Nodejs Bootcamp/Task-Manager/src/Routers/task.js
@@ -8,10 +8,13 @@ const auth = require('../middleware/auth')
 
 //! Task Routes
 //? Fetch all tasks
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        //const tasks = await Task.find({owner:req.user._id})
+        //res.send(tasks)
+
+        const user = await req.user.populate('tasks').execPopulate()
+        res.send(user.tasks)  
     } catch (error) {
         res.status(500).send()
     }
@@ -40,10 +43,11 @@ router.post('/tasks', auth, async (req, res)=>{
 })
 
 //? Fetch task by id
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id',auth , async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findById(_id)
+        //? Find Task by _id and it's user id
+        const task = await Task.findOne({_id, owner: req.user._id})
         if (!task) {
             return res.status(404).send()
         }
@@ -91,10 +95,10 @@ router.patch('/tasks/:id', async (req, res) => {
 })
 
 //? Deleting tasks
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
         const _id = req.params.id
-        const task = await Task.findByIdAndDelete(_id)
+        const task = await Task.findOneAndDelete({_id, owner: req.user._id})
         if (!task) {
             return res.status(404).send()
         }
