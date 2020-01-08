5fad7d725f7733c87c8dc2e20a4bb3684562b70f
Jeremy Linux Workstation <jeremyzelaya@hotmail.es>
2020-01-08 01:50:30 -0600

    Finished class on cascade deleting tasks with middleware and fixed patch route

--- Courses/Nodejs Bootcamp/Task-Manager/src/Routers/task.js
+++ Courses/Nodejs Bootcamp/Task-Manager/src/Routers/task.js
@@ -70,7 +70,7 @@ router.get('/tasks/:id',auth , async (req, res) => {
 
 
 //? Update Task
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const allowedUpdates = ['description', 'completed']
     const updates = Object.keys(req.body)
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
@@ -80,14 +80,14 @@ router.patch('/tasks/:id', async (req, res) => {
     try {
         const _id = req.params.id
         
-        const task = await Task.findById(_id)
-        updates.forEach(update => task[update] = req.body[update])
-        await task.save()
-        
+        const task = await Task.findOne({_id, owner: req.user._id})      
         //const task = await Task.findByIdAndUpdate(_id, req.body, { new:true, runValidators: true})
+        
         if (!task) {
             return res.status(404).send()
         }
+        updates.forEach(update => task[update] = req.body[update])
+        await task.save()
         res.send(task)
     } catch (error) {
         res.status(400).send()
