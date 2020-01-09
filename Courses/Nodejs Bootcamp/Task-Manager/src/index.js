e9c8639ef1aa73b26b99004f940b416386f728a8
Jeremy Linux Workstation <jeremyzelaya@hotmail.es>
2020-01-08 23:30:05 -0600

    Finished class on file uploads

--- Courses/Nodejs Bootcamp/Task-Manager/src/index.js
+++ Courses/Nodejs Bootcamp/Task-Manager/src/index.js
@@ -9,24 +9,23 @@ const auth = require('./middleware/auth')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method) {
-//         res.status(503).send('Server is under maintenance')
-//     }else{
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     if (req.method === "GET") {
-//         console.log('You cant use this operation')
-//         res.send('GET requests are disabled')    
-//     } else {
-//     console.log(req.method, req.path);
-//     //! Finish middleware function
-//     next()
-//     }
-// })
+const multer = require('multer')
+//? Multer config, destination folder is set
+const upload = multer({
+    dest: 'avatars'
+})
+
+//* Route for uploading using the multer middleware, here we set it to look for a single file named upload
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
+app.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
+})
+
+
 
 //! Parse JSON in a object
 app.use(express.json())
@@ -40,17 +39,4 @@ app.listen(PORT, () => {
     console.log('Server is up on port: ', PORT);
 })
 
-// const Task = require('./models/task')
-// const User = require('./models/user')
 
-// const main = async() => {
-//     // const task = await Task.findById('5e15772cd7a3b411db450dc7')
-//     // //? Populate the property with the relationship from User
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner); //? User information is displayed
-    
-//     const user = await User.findById('5e1576030e022b0fdaab3897')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks);
-// }
-// main()
