8e4175387e68b4831ddb4120cb25829538ad89f0
Jeremy Linux Workstation <jeremyzelaya@hotmail.es>
2020-01-15 23:46:15 -0600

    Finished class introduction on socket.io

--- Courses/Nodejs Bootcamp/chat-app/index.js
+++ Courses/Nodejs Bootcamp/chat-app/index.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
+const server = http.createServer(app)
+const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
 const localPublicPath = path.join(__dirname, './src/public')
 
 app.use(express.static(localPublicPath))
+
+//? When a new connection is started by the client
+io.on('connection', () => {
+    console.log('New web socket connection');
+    
+})
+
 app.get('/', (req, res) => {
 
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('Express server is up on Port:', PORT);
 })
